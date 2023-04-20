3b38763104dc44b57dc67c70d12530ecd00f90de
Vladislav Korzhov <korzhov.vladd@gmail.com>
2023-04-20 16:54:24 +0700

    add test to single prod

--- my-shop/python-app/main.py
+++ my-shop/python-app/main.py
@@ -3,27 +3,31 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Путь к файлу products.json
+# Путь к файлам
 PRODUCTS_FILE_PATH = '../public/architektoria-mocks/products.json'
-PRODUCTS_FILE_PATH = '../public/architektoria-mocks/products.json'
+MATERIALS_FILE_PATH = '../public/architektoria-mocks/materials.json'
+OPTIONS_FILE_PATH = '../public/architektoria-mocks/options.json'
+BASE_FILE_PATH = '../public/architektoria-mocks/base.json'
+SIZES_FILE_PATH = '../public/architektoria-mocks/sizes.json'
 
-def load_products():
-    with open(PRODUCTS_FILE_PATH, 'r', encoding='utf-8') as file:
-        products_data = json.load(file)
-    return products_data
+
+def load_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data
 
 
 # Эндпоинт для получения списка товаров
 @app.route('/api/products', methods=['GET'])
 def get_products():
-    products = load_products()
+    products = load_file(PRODUCTS_FILE_PATH)
     return jsonify(products)
 
 
 # Эндпоинт для получения деталей о товаре по ID или Slug
 @app.route('/api/products/<string:identifier>', methods=['GET'])
 def get_product(identifier):
-    products = load_products()
+    products = load_file(PRODUCTS_FILE_PATH)
     product = None
     for p in products:
         if str(p["id"]) == identifier or p["slug"] == identifier:
@@ -32,15 +36,18 @@ def get_product(identifier):
     if product:
         product['image'] = "https://api.architektoria.ru" + product['image']
         product.update({"properties": {
-            "materials": [],
-            "options": [],
-            "base": [],
-            "sizes": []
-        }})
+            "materials": load_file(MATERIALS_FILE_PATH),
+            "options": load_file(OPTIONS_FILE_PATH),
+            "base": load_file(BASE_FILE_PATH),
+            "sizes": load_file(SIZES_FILE_PATH)
+            }
+            }
+        )
 
         return jsonify(product)
     else:
         return jsonify({"error": "Продукт не найден"}), 404
 
 if __name__ == '__main__':
+    load_file(MATERIALS_FILE_PATH)
     app.run(debug=True)
--- my-shop/python-app/test.py
+++ my-shop/python-app/test.py
@@ -1,6 +1,6 @@
 import requests
 import json
 
-answ = requests.get("http://127.0.0.1:5000/api/products")
+answ = requests.get("http://127.0.0.1:5000/api/products/olimpiya")
 
 print(answ.json())
